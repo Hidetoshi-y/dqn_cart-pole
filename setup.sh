3b1a5f75851bdff0c2676a61fa338a216cb74c4e
yoshida-Hidetoshi <sky11fur@gmail.com>
2020-06-30 15:06:34 +0900

    Modfy .gitignore setup.sh Make check_tensor.py

--- setup.sh
+++ setup.sh
@@ -6,7 +6,10 @@ else
     pip install tensorflow==1.13.1 || exit 1
 fi
 
+
 pip install gym==0.13.1 || exit 1
 pip install keras==2.2.4 || exit 1
 pip install keras-rl==0.4.2 || exit 1
 pip install gym[atari]==0.13.1 || exit 1
+
+#pip install tensorflow-gpu==1.13.1 || exit 1
