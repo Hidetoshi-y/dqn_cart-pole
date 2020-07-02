04a396b8d5253d0df7648c462f8b3c154955078e
yoshida-Hidetoshi <sky11fur@gmail.com>
2020-07-02 10:26:53 +0900

    Modyfi setup.sh use cpu keras-rl and tensorflow .gitignore add name

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-
-if type nvidia-smi >/dev/null 2>&1 && [ x"$(nvidia-smi -L)" != x ]; then
-    pip install tensorflow-gpu==1.13.1 || exit 1
-else
-    pip install tensorflow==1.13.1 || exit 1
-fi
+#!/bin/bash -e
 
 
-pip install gym==0.13.1 || exit 1
-pip install keras==2.2.4 || exit 1
-pip install keras-rl==0.4.2 || exit 1
-pip install gym[atari]==0.13.1 || exit 1
+#if [ ! -f /.dockerenv -a x"$NVIDIA_BUILD_ID" == x ]; then
+    #if type nvidia-smi >/dev/null 2>&1 && [ x"$(nvidia-smi -L)" != x ]; then
+        #pip install tensorflow-gpu==1.13.1 
+    #else
+        #pip install tensorflow==1.13.1 
+    #fi
+#fi
 
-#pip install tensorflow-gpu==1.13.1 || exit 1
+pip install gym==0.13.1 
+pip install keras==2.2.4 
+pip install keras-rl==0.4.2 
+pip install gym[atari]==0.13.1 
+pip install tensorflow==1.13.1
